13f61e015f11d4096ec4e5917c8ab89bcebf4b0d
Mihail Polimenov <Mistpolimen91@gmail.com>
2023-03-02 00:00:00 +0200

    made 05.js with arrow functions

--- Functions and Statements/lab/05.js
+++ Functions and Statements/lab/05.js
@@ -1,11 +1,16 @@
 function calculator (numOne, numTwo, operator) {
-    let output = 0;
+    
+    const sum = (a, b) => a + b;
+    const subtract = (a, b) => a - b;
+    const divide = (a, b) => a / b;
+    const multiply = (a, b) => a * b;
+
     switch(operator) {
-        case 'multiply': {console.log(numOne * numTwo)}; break;
-        case 'divide':  {console.log(numOne / numTwo)}; break;
-        case 'add':  {console.log(numOne + numTwo)}; break;
-        case 'subtract':  {console.log(numOne - numTwo)};
+        case 'multiply': return multiply(numOne, numTwo);
+        case 'divide': return divide(numOne, numTwo);
+        case 'add': return sum(numOne, numTwo);
+        case 'subtract': return subtract(numOne, numTwo);
     }  
 }
 
-calculator(2, 3, 'multiply');
+console.log(calculator(2, 3, 'multiply'));
