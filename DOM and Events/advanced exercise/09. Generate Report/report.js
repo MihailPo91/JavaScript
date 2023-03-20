219d8e086e03a03ca1f8333bcd3c7e83ecf3251a
Mihail Polimenov <Mistpolimen91@gmail.com>
2023-03-20 22:52:09 +0200

    some more ex

--- DOM and Events/advanced exercise/09. Generate Report/report.js
+++ DOM and Events/advanced exercise/09. Generate Report/report.js
@@ -1,3 +1,38 @@
 function generateReport() {
-    //TODO
+    const checkedHeaderCells = Array.from(document.querySelectorAll('input:checked'));
+    let properties = [];
+    for (const input of checkedHeaderCells) {
+        properties.push(input.parentElement.textContent.trim().toLowerCase());
+    }
+
+    const rows = Array.from(document.getElementsByTagName('tr')).splice(1);
+    const result = document.getElementById('output');
+
+
+    let output = [];
+
+    rows.forEach((row) => {
+        let newObject = {};
+        let rowChildren = Array.from(row.children);
+        for (const prop of properties) {
+            if (prop === 'employee') {
+                newObject['employee'] = rowChildren[0].textContent;
+            } else if (prop === 'department') {
+                newObject['department'] = rowChildren[1].textContent;
+            } else if (prop === 'status') {
+                newObject['status'] = rowChildren[2].textContent;
+            } else if (prop === 'date hired') {
+                newObject['date_hired'] = rowChildren[3].textContent;
+            } else if (prop === 'benefits') {
+                newObject['benefits'] = rowChildren[4].textContent;
+            } else if (prop === 'compensation') {
+                newObject['compensation'] = rowChildren[5].textContent;
+            } else if (prop === 'rating') {
+                newObject['rating'] = rowChildren[6].textContent;
+            }
+        }
+        output.push(newObject);
+    });
+    // output.forEach((object) => result.value += JSON.stringify(object));
+    result.value = JSON.stringify(output);
 }
--- DOM and Events/advanced exercise/10. Number Convertor/solution.js
+++ DOM and Events/advanced exercise/10. Number Convertor/solution.js
@@ -1,4 +1,29 @@
 function solve() {
+    const convertBtn = document.getElementsByTagName('button')[0];
+    convertBtn.addEventListener('click', convert);
 
-    //TODO...
+    document.getElementsByTagName('option')[1].remove();
+    let newOptBin = document.createElement('option');
+    newOptBin.value = 'binary';
+    newOptBin.textContent = 'Binary';
+    let newOptHex = document.createElement('option');
+    newOptHex.value = 'hexadecimal';
+    newOptHex.textContent = 'Hexadecimal';
+    document.getElementById('selectMenuTo').appendChild(newOptBin);
+    document.getElementById('selectMenuTo').appendChild(newOptHex);
+
+
+    function convert() {
+        const input = document.getElementsByTagName('input')[0].value;
+        const typeConvertion = document.getElementById('selectMenuTo');
+        const result = document.getElementsByTagName('input')[1];
+        let output = '';
+
+        if (typeConvertion.value === 'binary') {
+            output = Number(input).toString(2);
+        } else if (typeConvertion.value === 'hexadecimal') {
+            output = Number(input).toString(16).toUpperCase();
+        }
+        result.value = output;
+    }
 }
