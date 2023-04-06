abe26634f29c3ff1b9e4d7811501432811e368ee
Mihail Polimenov <Mistpolimen91@gmail.com>
2023-04-06 23:27:14 +0300

    12 + 13 from advanced ex DOM

--- DOM and Events/advanced exercise/12. Encode-and-Decode-Messages/app.js
+++ DOM and Events/advanced exercise/12. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const allTextAreas = Array.from(document.getElementsByTagName('textarea'));
+    const messageTextArea = allTextAreas[0];
+    const receiverTextArea = allTextAreas[1];
+
+    const encodeSendBtn = Array.from(document.getElementsByTagName('button'))[0];
+    const decodeReadBtn = Array.from(document.getElementsByTagName('button'))[1];
+
+    encodeSendBtn.addEventListener('click', encodeMessageHandler);
+    decodeReadBtn.addEventListener('click', decodeAndReadMessaveHandler);
+
+
+    function encodeMessageHandler() {
+        let message = messageTextArea.value.split('');
+        let result = '';
+
+        for (const char of message) {
+            let asciiCode = char.charCodeAt(0) + 1;
+            let newChar = String.fromCharCode(asciiCode);
+            result += newChar;
+        }
+
+        receiverTextArea.value = result;
+        messageTextArea.value = '';
+    }
+
+    function decodeAndReadMessaveHandler() {
+        let message = receiverTextArea.value.split('');
+        let result = '';
+
+        for (const char of message) {
+            let asciiCode = char.charCodeAt(0) - 1;
+            let newChar = String.fromCharCode(asciiCode);
+            result += newChar;
+        }
+
+        receiverTextArea.value = result;
+    }
 }
--- DOM and Events/advanced exercise/13. Distance-Converter/distanceConverter.js
+++ DOM and Events/advanced exercise/13. Distance-Converter/distanceConverter.js
@@ -1,3 +1,36 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const from = document.getElementById('inputUnits');
+    const to = document.getElementById('outputUnits');
+
+    const inputField = document.getElementById('inputDistance');
+    console.log(inputField)
+    const outputField = document.getElementById('outputDistance');
+
+    const convertBtn = document.getElementById('convert');
+    convertBtn.addEventListener('click', convertHandler);
+
+    const ratesInMeters = {
+        'km': 1000,
+        'm': 1,
+        'cm': 0.01,
+        'mm': 0.001,
+        'mi': 1609.34,
+        'yrd': 0.9144,
+        'ft': 0.3048,
+        'in': 0.0254
+    }
+
+    function convertHandler() {
+        const fromUnits = from.value;
+        const toUnits = to.value;
+
+        console.log(ratesInMeters[fromUnits]);
+        console.log(ratesInMeters[toUnits]);
+
+        let amount = Number(inputField.value);
+        console.log(amount)
+
+        let result = (amount * ratesInMeters[fromUnits]) / ratesInMeters[toUnits];
+        outputField.value = result;
+    }
 }
